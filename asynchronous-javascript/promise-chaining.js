cd670fee10b30ef10b0809c66d581ebe7b1b1bec
damonholden <damonh003@gmail.com>
2022-11-26 18:22:10 +0000

    feat: add knowledge on promise chaining

---
+++ asynchronous-javascript/promise-chaining.js
@@ -0,0 +1,24 @@
+// Promise methods can be chained. This is especially useful when you need to do
+// further promise-based computation with the resolution of a previous promise.
+
+{
+  // fetch('https://www.boredapi.com/api/activity')
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(`💡 ${data.activity}`));
+
+  // console.log('🎃 synchronous');
+}
+
+// All potential errors within a promise chain can be captured with a single
+// .catch() method:
+
+{
+  fetch('https://httpstat.us/404')
+    .then((response) => response.json())
+    .then((data) => console.log(`💡 ${data.activity}`))
+    .catch((error) => console.error(`UH-OH! ${error}`));
+
+  console.log('🎃 synchronous');
+}
+
+// https://youtu.be/vn3tm0quoqE
