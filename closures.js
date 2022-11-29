1bc6e9d859bf9771b6f2d4b13ee6df589a493485
damonholden <damonh003@gmail.com>
2022-11-29 22:16:16 +0000

    feat: add learning on closure

---
+++ closures.js
@@ -0,0 +1,13 @@
+// a closure is simply a function with reference to its surrounding state (also
+// known as the lexical environment). Closures are created automatically whenever
+// a function is created and allows the function to access its outer-scope. You
+// most likely know what a closure is and how to use it, but just haven't given
+// it a name.
+
+function doesSomething() {
+	const name = 'John'
+
+	function logName() {
+		console.log(name); // name can be accessed here because `logName` has a closure
+	}
+}
