37f2ba082f4b40e5245f612036a2ac647ba5b649
damonholden <damonh003@gmail.com>
2022-12-18 23:55:19 +0000

    feat: add learning on with statement

---
+++ with.js
@@ -0,0 +1,6 @@
+// the `with` extends the scope chain with the properties of the object
+
+// do not use `with` in your code
+
+// learn more:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with
